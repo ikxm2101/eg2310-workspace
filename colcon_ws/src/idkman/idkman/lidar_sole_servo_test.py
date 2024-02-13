ffe1c81d76955af7d735bce854d08650b3aed7bc
Irwin <irwin.kxm@outlook.com>
2024-02-13 12:41:13 +0000

    added delay for hardware colcon_ws/src/idkman

--- colcon_ws/src/idkman/idkman/lidar_sole_servo_test.py
+++ colcon_ws/src/idkman/idkman/lidar_sole_servo_test.py
@@ -9,6 +9,7 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
 
+import time
 import numpy as np
 
 from .servo_device import Servo_device
@@ -31,6 +32,7 @@ class Scanner(Node):
       qos_profile_sensor_data)
     self.subscription  # prevent unused variable warning
     
+    # Variables/flag to detect minimum distance
     self.mindist = 0.3
     self.mindist_detected = False
 
@@ -41,28 +43,34 @@ class Scanner(Node):
     laser_range[laser_range == 0] = np.nan
 
     measured_mindist = np.nanmin(laser_range)
-    print(measured_mindist)
+    print("Measured minimum distance is:", measured_mindist)
 
     if measured_mindist <= self.mindist and not self.mindist_detected:
       self.mindist_detected = True
       servo.set_angle(45)
       solenoidsw.on()
+      time.sleep(1)
     
     if measured_mindist > self.mindist and self.mindist_detected:
       self.mindist_detected = False
       servo.reset()
       solenoidsw.off()
+      time.sleep(1)
 
 def main(args=None):
-  rclpy.init(args=args)
-  scanner = Scanner()
-  rclpy.spin(scanner)
+  try:
+    rclpy.init(args=args)
+    scanner = Scanner()
+    rclpy.spin(scanner)
 
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
-  scanner.destroy_node()
-  rclpy.shutdown()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    scanner.destroy_node()
+    rclpy.shutdown()
+  except KeyboardInterrupt:
+    servo.close()
+    solenoidsw.close()
 
 if __name__ == '__main__':
   main()
